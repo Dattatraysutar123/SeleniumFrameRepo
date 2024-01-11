9f0a08965c64b0ecab6a92f59de115333b246181
Sutar Dattatray <sutardattatray123@gmail.com>
2024-01-11 10:59:42 +0530

    second commit

--- src/test/java/tests/Test4.java
+++ src/test/java/tests/Test4.java
@@ -1,5 +1,7 @@
 package tests;
 
+import org.testng.annotations.Test;
+import org.testng.AssertJUnit;
 import java.io.IOException;
 
 import org.openqa.selenium.WebDriver;
@@ -9,20 +11,20 @@ import org.testng.annotations.Test;
 
 import resources.Base;
 
-public class Test4 extends Base{
+public class Test4<WebDriver> extends Base{
 	public WebDriver driver;
 @Test
 	public void testData() throws IOException, InterruptedException {
-		
+		System.out.println("Datta Has updated test 4 ");
 	  driver = initializeDriver();
 	  
-	  driver.get("http://tutorialsninja.com/demo/");
+	  ((Object) driver).get("http://tutorialsninja.com/demo/");
 	  
 	  System.out.println("Test two data verify");
 	  
 	  Thread.sleep(2000);
 	  
-	  Assert.assertTrue(false);
+	  AssertJUnit.assertTrue(false);
 	  
 	}
 
